2dcece291cba9205ffb23cc71ff37295c19800df
JC <39209141+jhc13@users.noreply.github.com>
2022-05-14 22:42:55 +0900

    Add state_dict and images

--- country-classifier/config.py
+++ country-classifier/config.py
@@ -11,11 +11,11 @@ MAX_EPOCH_COUNT = 100
 EARLY_STOPPING_PATIENCE = 5
 
 # test.py
-TEST_STATE_DICT_PATH = None
+TEST_STATE_DICT_PATH = '../models/state_dict.pt'
 
 # predict.py
-PREDICT_STATE_DICT_PATH = None
-PREDICT_IMAGE_PATH = None
+PREDICT_STATE_DICT_PATH = '../models/state_dict.pt'
+PREDICT_IMAGE_PATH = '../images/sweden.jpg'
 PREDICT_TOP_K = 5
 MAP_SHAPEFILE_PATH = '../shapefiles/ne_110m_admin_0_countries.shp'
-PLOT_SAVE_PATH = None
+PLOT_SAVE_PATH = '../images/sweden.svg'
